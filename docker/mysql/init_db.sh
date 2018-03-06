a04e0be4eb18c4e7bb0af08a602727f4ce12b2c5
Ignacio Cruz <igmoweb@gmail.com>
2018-03-06 13:10:40 +0100

    .

--- docker/mysql/init_db.sh
+++ docker/mysql/init_db.sh
@@ -2,4 +2,4 @@
 echo "---------------------------PREPOPULATING DATABASE---------------------------"
 /usr/bin/mysqld_safe --skip-grant-tables &
 sleep 5
-mysql -u root -p"somewordpress" wordpress < ./dump.sql
+mysql -u root -p"somewordpress" wordpress < /dump.sql
